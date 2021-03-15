fa2b1174558a4dd27a4d12359820d65c870c73fc
Hasanuzzaman <shuvo009@yahoo.com>
2021-03-15 22:35:12 +0600

    Product

---
+++ Grpc.DotNetCore.Common/DbModel/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Grpc.DotNetCore.Common.DbModel
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+    }
+}
---
+++ Grpc.DotNetCore.Common/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.DotNetCore.Common.DbModel;
+
+namespace Grpc.DotNetCore.Common.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<ProductModel> GetProduct(Guid id);
+    }
+}
