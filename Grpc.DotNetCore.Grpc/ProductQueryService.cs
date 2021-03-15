e99819a10f95c304fedbbf5c037ad89491a6d98e
Hasanuzzaman <shuvo009@yahoo.com>
2021-03-15 22:50:39 +0600

    Grpc

---
+++ Grpc.DotNetCore.Grpc/ProductQueryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.DotNetCore.Common.Interfaces;
+using Grpc.DotNetCore.Grpc.Protos;
+
+namespace Grpc.DotNetCore.Grpc
+{
+    public class ProductQueryService : ProductQueryGrpcService.ProductQueryGrpcServiceBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductQueryService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public override async Task<ProductQueryResponse> GetProduct(ProductQueryRequest request,
+            ServerCallContext context)
+        {
+            var productId = Guid.Parse(request.Id);
+            var product = await _productRepository.GetProduct(productId);
+            return new ProductQueryResponse
+            {
+                Label = product.Label,
+                Name = product.Name
+            };
+        }
+    }
+}
