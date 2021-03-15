8b80716079671c369d086c9cfd34ea017a4d2070
Hasanuzzaman <shuvo009@yahoo.com>
2021-03-15 22:42:03 +0600

    Repository

---
+++ Grpc.DotNetCore.Repository/Data/ProductDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.DotNetCore.Common.DbModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grpc.DotNetCore.Repository.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
---
+++ Grpc.DotNetCore.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.DotNetCore.Common.DbModel;
+using Grpc.DotNetCore.Common.Interfaces;
+using Grpc.DotNetCore.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grpc.DotNetCore.Repository.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DbSet<ProductModel> _model;
+
+        public ProductRepository(ProductDbContext profileDbContext)
+        {
+            _model = profileDbContext.Products;
+        }
+
+        public Task<ProductModel> GetProduct(Guid id)
+        {
+            return _model.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
